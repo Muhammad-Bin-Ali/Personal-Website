237ea0c40caed566fe4cec24b1d8d111c4d59592
Muhammad-Bin-Ali <muhammadbinali706@gmail.com>
2023-02-14 01:21:49 -0500

    moved files

--- server/server/myapi/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets 
-from .serializers import ProjectSerializer
-from .models import Project
-
-#Viewsets only offer retrieve and list methods. Makes it easier to handle GET requests
-class ProjectViewSet(viewsets.ModelViewSet):
-    serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
-
